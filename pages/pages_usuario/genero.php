1fa0432be1b59e2fbab7671c2c3adc77b162fcc9
guilherme_koerber@joinville.sc.senai.br <guilherme_koerber@joinville.sc.senai.br>
2019-09-25 22:13:17 -0300

    Update genero.php

--- pages/pages_usuario/genero.php
+++ pages/pages_usuario/genero.php
@@ -1,5 +1,3 @@
-<<<<<<< Updated upstream
-=======
 <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
 <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
 <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
@@ -51,4 +49,3 @@
 
     </body>
 </html>
->>>>>>> Stashed changes
