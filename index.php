0991f418886a8195be4b164593d5e5da456e200a
Bruna dos Santos Ribeiro <54964052+Bruna0001@users.noreply.github.com>
2019-10-01 19:18:40 -0300

    Login pronto com css
    Login pronto com css

--- index.php
+++ index.php
@@ -10,8 +10,47 @@ if(isset($_GET["erro"])){
 	}
 }
 ?>
-
 <!DOCTYPE html>
+<html>
+<head>
+ <title>Feels Music</title>
+ <meta charset='utf-8'/>
+ <meta name='author' content="Bruna Ribeiro, Felipe Rangel, Guilherme Koerber e Roberto Casagrande.">
+ <link rel="stylesheet" href="css/bootstrap.min.css" />
+ <style media="screen">
+	 body{
+		 background-color: #171717;
+	 }
+	 h1{
+		 color: #FC9F01
+	 }
+ </style>
+</head>
+<body>
+ <div class="container">
+	 <div class="mx-auto" style="width:200px;">
+		 <br>
+		 <img class="logo" src="assets/imgs/Icon.png"/ width="200" height="250">
+		 <h1> Login </h1>
+		 <form action="includes/validarLogin.php" name="formlogin" method="post">
+			 <div class="form-group">
+					<input type="text" class="form-control" id="login" name="login" placeholder="Nickname" required>
+			 </div>
+			 <div class="form-group">
+					<input type="password" class="form-control" id="senha" name="senha" placeholder="Senha" required>
+			 </div>
+				 <button type="submit" style="background-color: #FC9F01;" class="btn btn-warning">Entrar</button>
+
+		 </form>
+	 </div>
+ </div>
+ <footer>
+ </footer>
+</body>
+</html>
+
+
+<!-- <!DOCTYPE html>
 <html>
   <head>
     <title>Feels Music</title>
@@ -30,4 +69,4 @@ if(isset($_GET["erro"])){
     <footer>
     </footer>
   </body>
-</html>
+</html> -->
